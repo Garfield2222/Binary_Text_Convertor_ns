c41fe79a126ba324c6a49937e0d52ca08418352f
Garfield2222 <garfield.nembhard@gmail.com>
2023-01-27 10:24:48 -0500

    Add project files.

---
+++ Binary_Text_Convertor/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace Binary_Text_Convertor
+{
+    partial class Binary_And_Text_Conversion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BinaryDigitsLbl = new System.Windows.Forms.Label();
+            this.TextLbl = new System.Windows.Forms.Label();
+            this.BinaryDigitsTbx = new System.Windows.Forms.TextBox();
+            this.TextDisplayTbx = new System.Windows.Forms.TextBox();
+            this.Convert_to_Text_btn = new System.Windows.Forms.Button();
+            this.ClearBtn = new System.Windows.Forms.Button();
+            this.Convert_To_Binary_btn = new System.Windows.Forms.Button();
+            this.TitleLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BinaryDigitsLbl
+            // 
+            this.BinaryDigitsLbl.AutoSize = true;
+            this.BinaryDigitsLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BinaryDigitsLbl.Location = new System.Drawing.Point(36, 105);
+            this.BinaryDigitsLbl.Name = "BinaryDigitsLbl";
+            this.BinaryDigitsLbl.Size = new System.Drawing.Size(126, 25);
+            this.BinaryDigitsLbl.TabIndex = 0;
+            this.BinaryDigitsLbl.Text = "Binary Digits:";
+            // 
+            // TextLbl
+            // 
+            this.TextLbl.AutoSize = true;
+            this.TextLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextLbl.Location = new System.Drawing.Point(36, 308);
+            this.TextLbl.Name = "TextLbl";
+            this.TextLbl.Size = new System.Drawing.Size(57, 25);
+            this.TextLbl.TabIndex = 1;
+            this.TextLbl.Text = "Text:";
+            // 
+            // BinaryDigitsTbx
+            // 
+            this.BinaryDigitsTbx.Location = new System.Drawing.Point(184, 108);
+            this.BinaryDigitsTbx.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.BinaryDigitsTbx.Multiline = true;
+            this.BinaryDigitsTbx.Name = "BinaryDigitsTbx";
+            this.BinaryDigitsTbx.Size = new System.Drawing.Size(349, 182);
+            this.BinaryDigitsTbx.TabIndex = 2;
+            // 
+            // TextDisplayTbx
+            // 
+            this.TextDisplayTbx.Location = new System.Drawing.Point(184, 311);
+            this.TextDisplayTbx.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.TextDisplayTbx.Multiline = true;
+            this.TextDisplayTbx.Name = "TextDisplayTbx";
+            this.TextDisplayTbx.Size = new System.Drawing.Size(349, 157);
+            this.TextDisplayTbx.TabIndex = 3;
+            // 
+            // Convert_to_Text_btn
+            // 
+            this.Convert_to_Text_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Convert_to_Text_btn.Location = new System.Drawing.Point(41, 502);
+            this.Convert_to_Text_btn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Convert_to_Text_btn.Name = "Convert_to_Text_btn";
+            this.Convert_to_Text_btn.Size = new System.Drawing.Size(175, 41);
+            this.Convert_to_Text_btn.TabIndex = 4;
+            this.Convert_to_Text_btn.Text = "Convert To Text";
+            this.Convert_to_Text_btn.UseVisualStyleBackColor = true;
+            this.Convert_to_Text_btn.Click += new System.EventHandler(this.Convert_to_Text_btn_Click);
+            // 
+            // ClearBtn
+            // 
+            this.ClearBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ClearBtn.Location = new System.Drawing.Point(433, 502);
+            this.ClearBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.ClearBtn.Name = "ClearBtn";
+            this.ClearBtn.Size = new System.Drawing.Size(100, 41);
+            this.ClearBtn.TabIndex = 5;
+            this.ClearBtn.Text = "Clear";
+            this.ClearBtn.UseVisualStyleBackColor = true;
+            this.ClearBtn.Click += new System.EventHandler(this.ClearBtn_Click);
+            // 
+            // Convert_To_Binary_btn
+            // 
+            this.Convert_To_Binary_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Convert_To_Binary_btn.Location = new System.Drawing.Point(223, 502);
+            this.Convert_To_Binary_btn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Convert_To_Binary_btn.Name = "Convert_To_Binary_btn";
+            this.Convert_To_Binary_btn.Size = new System.Drawing.Size(203, 41);
+            this.Convert_To_Binary_btn.TabIndex = 6;
+            this.Convert_To_Binary_btn.Text = "Convert To Binary";
+            this.Convert_To_Binary_btn.UseVisualStyleBackColor = true;
+            this.Convert_To_Binary_btn.Click += new System.EventHandler(this.Convert_To_Binary_btn_Click);
+            // 
+            // TitleLbl
+            // 
+            this.TitleLbl.AutoSize = true;
+            this.TitleLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TitleLbl.Location = new System.Drawing.Point(107, 11);
+            this.TitleLbl.Name = "TitleLbl";
+            this.TitleLbl.Size = new System.Drawing.Size(351, 31);
+            this.TitleLbl.TabIndex = 7;
+            this.TitleLbl.Text = "Binary And Text Conversion";
+            // 
+            // Binary_And_Text_Conversion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(589, 585);
+            this.Controls.Add(this.TitleLbl);
+            this.Controls.Add(this.Convert_To_Binary_btn);
+            this.Controls.Add(this.ClearBtn);
+            this.Controls.Add(this.Convert_to_Text_btn);
+            this.Controls.Add(this.TextDisplayTbx);
+            this.Controls.Add(this.BinaryDigitsTbx);
+            this.Controls.Add(this.TextLbl);
+            this.Controls.Add(this.BinaryDigitsLbl);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Binary_And_Text_Conversion";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Binary And Text Conversion";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label BinaryDigitsLbl;
+        private System.Windows.Forms.Label TextLbl;
+        private System.Windows.Forms.TextBox BinaryDigitsTbx;
+        private System.Windows.Forms.TextBox TextDisplayTbx;
+        private System.Windows.Forms.Button Convert_to_Text_btn;
+        private System.Windows.Forms.Button ClearBtn;
+        private System.Windows.Forms.Button Convert_To_Binary_btn;
+        private System.Windows.Forms.Label TitleLbl;
+    }
+}
+
---
+++ Binary_Text_Convertor/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Binary_Text_Convertor
+{
+    public partial class Binary_And_Text_Conversion : Form
+    {
+        public Binary_And_Text_Conversion()
+        {
+            InitializeComponent();
+        }
+
+        private void Convert_to_Text_btn_Click(object sender, EventArgs e)
+        {
+            string tempString = "";
+            string Character = System.Text.RegularExpressions.Regex.Replace(BinaryDigitsTbx.Text, "[^01]", "");
+
+            byte[] Bytes = new byte[(Character.Length / 8) - 1 + 1];
+
+            for (int i = 0; i <= Bytes.Length - 1; i++)
+            {
+                Bytes[i] = Convert.ToByte(Character.Substring(i * 8, 8), 2);
+            }
+
+            tempString = (string)(System.Text.ASCIIEncoding.ASCII.GetString(Bytes));
+
+            TextDisplayTbx.Text = tempString;
+        }
+
+        private void ClearBtn_Click(object sender, EventArgs e)
+        {
+            BinaryDigitsTbx.Text = String.Empty;
+            TextDisplayTbx.Text = String.Empty;
+        }
+
+        private void Convert_To_Binary_btn_Click(object sender, EventArgs e)
+        {
+            string tempString = "";
+            System.Text.StringBuilder txtBuilder = new System.Text.StringBuilder();
+
+            foreach (byte Character in System.Text.ASCIIEncoding.ASCII.GetBytes(TextDisplayTbx.Text))
+            {
+                txtBuilder.Append(Convert.ToString(Character, 2).PadLeft(8, '0'));
+                txtBuilder.Append(" ");
+            }
+
+            tempString = txtBuilder.ToString().Substring(0, txtBuilder.ToString().Length - 0);
+
+            BinaryDigitsTbx.Text = tempString;
+        }
+    }
+}
